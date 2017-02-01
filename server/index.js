6aa41b5f8da051096356bf9df5e43e2e6cb954a8
Thomas <Thomas@DevBook>
2017-02-01 21:12:58 +0100

    save previous id in cookie and load this instead of generating a new one

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ var config = require('./config.json')[process.env.NODE_ENV || 'production'];
 var port = process.env.PORT || config.port;
 var express = require("express");
 var app = express();
-// var bodyParser = require("body-parser");
+var cookieParser = require('cookie-parser');
 var http = require("http").Server(app);
 var socketIo = require('socket.io')(http, {
     pingTimeout: 2000,
@@ -35,45 +35,6 @@ var socketIo = require('socket.io')(http, {
 var documentsFile = path.resolve(__dirname + '/documents.json');
 var documents = {};
 var saveInterval;
-
-
-
-// var nconf = require('nconf');
-// nconf.file(path.resolve(__dirname + '/auth.json')).env();
-// var authConfig = {
-//     clientID: nconf.get('clientID'),
-//     clientSecret: nconf.get('clientSecret'),
-//     callbackURL: nconf.get('callbackURL')
-// };
-// var passport = require("passport");
-
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-
-// function extractProfile(profile) {
-//     let imageUrl = '';
-//     if (profile.photos && profile.photos.length) {
-//         imageUrl = profile.photos[0].value;
-//     }
-//     return {
-//         id: profile.id,
-//         displayName: profile.displayName,
-//         image: imageUrl
-//     };
-// }
-
-// passport.serializeUser((user, cb) => {
-//     cb(null, user);
-// });
-// passport.deserializeUser((obj, cb) => {
-//     cb(null, obj);
-// });
-// passport.use(new GoogleStrategy(authConfig, function (token, refreshToken, profile, done) {
-//     cb(null, extractProfile(profile));
-// }));
-
 
 
 if (file.exist(documentsFile)) {
@@ -100,15 +61,7 @@ function stopSaveTimer() {
     clearInterval(saveInterval);
 }
 
-// app.get('/auth', passport.authenticate('google', { scope: ['profile', 'email'] }));
-
-// app.get("/clear", function (req, res) {
-//     stopSaveTimer();
-//     documents = {};
-//     file.write(documentsFile, JSON.stringify(documents));
-//     startSaveTimer();
-//     return res.redirect('/');
-// });
+app.use(cookieParser());
 
 app.use("/d/:id?", function (req, res, next) {
     if (!req.params.id) {
@@ -118,7 +71,20 @@ app.use("/d/:id?", function (req, res, next) {
 }, express.static(path.resolve(__dirname + "/../public")));
 
 app.get("/", function (req, res) {
-    var id = makeid();
+    var id;
+    var previousId = req.cookies.scribbleId;
+
+    if (previousId) {
+        id = previousId;
+    }
+    else {
+
+        id = makeid();
+        res.cookie('scribbleId', id, {
+            expires: new Date(Date.now() + 3 * 365 * 24 * 60 * 60 * 1000),
+            httpOnly: false //otherwise it cannot be deleted by js on client side
+        });
+    }
     return res.redirect('/d/' + id);
 });
 
