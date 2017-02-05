810ad009ec3ebae577aaeeedef2c4cdd7402554a
Thomas <Thomas@DevBook>
2017-02-05 13:35:56 +0100

    upload draft

--- public/main.js
+++ public/main.js
@@ -64,3 +64,95 @@ socket.on('delete', function (e) {
         window.location = '/';
     }
 });
+
+
+$('body').bind('dragover', function (e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    if (e.originalEvent.dataTransfer.items) {
+        $('#dropArea > h4').text('Dateien und Ordner hier ablegen, um sie der Upload-Liste hinzuzufügen');
+    } else {
+        $('#dropArea > h4').text('Dateien hier ablegen, um sie der Upload-Liste hinzuzufügen');
+    }
+
+    $('#dropArea').addClass('visible');
+    e.originalEvent.dataTransfer.dropEffect = 'copy';
+});
+
+$('#dropArea').bind('dragleave', function (e) {
+    $('#dropArea').removeClass('visible');
+});
+
+$('#dropArea').bind('drop', function (e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    let files;
+
+    // if (e.originalEvent.dataTransfer.items) {
+    //     files = e.originalEvent.dataTransfer.items;
+    //     // for (var i = 0; i < e.dataTransfer.items.length; i++) {
+    //     //     var item = e.dataTransfer.items[i].webkitGetAsEntry();
+    //     //     if (item) {
+    //     //         processSubdirectory(item);
+    //     //     }
+    //     // }
+    // } else if (e.originalEvent.dataTransfer.files) {
+        files = e.originalEvent.dataTransfer.files;
+    // }
+
+    console.dir(files);
+
+    if (files.length > 0) {
+        var formData = new FormData();
+        for (var i = 0; i < files.length; i++) {
+            var file = files[i];
+            console.log(file.size);
+            formData.append('uploads[]', file, file.name);
+        }
+
+        $.ajax({
+            url: '/upload',
+            type: 'POST',
+            data: formData,
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: function (data) {
+                console.log('upload successful!');
+            },
+            xhr: function () {
+                // create an XMLHttpRequest
+                var xhr = new XMLHttpRequest();
+
+                // listen to the 'progress' event
+                xhr.upload.addEventListener('progress', function (evt) {
+
+                    if (evt.lengthComputable) {
+                        // calculate the percentage of upload completed
+                        var percentComplete = evt.loaded / evt.total;
+                        percentComplete = parseInt(percentComplete * 100);
+
+                        console.log(percentComplete + '%');
+                        // update the Bootstrap progress bar with the new percentage
+                        // $('.progress-bar').text(percentComplete + '%');
+                        // $('.progress-bar').width(percentComplete + '%');
+
+                        // once the upload reaches 100%, set the progress bar text to done
+                        if (percentComplete === 100) {
+                            console.log("done");
+                            // $('.progress-bar').html('Done');
+                        }
+
+                    }
+
+                }, false);
+
+                return xhr;
+            }
+        });
+    }
+
+    $('#dropArea').removeClass('visible');
+});
--- server/index.js
+++ server/index.js
@@ -25,6 +25,8 @@ var file = require('./file')();
 var config = require('./config.json')[process.env.NODE_ENV || 'production'];
 var port = process.env.PORT || config.port;
 var express = require("express");
+var formidable = require('formidable');
+var fs = require('fs');
 var app = express();
 var cookieParser = require('cookie-parser');
 var http = require("http").Server(app);
@@ -62,6 +64,27 @@ function stopSaveTimer() {
 }
 
 app.use(cookieParser());
+
+app.post("/upload", function (req, res) {
+    var form = new formidable.IncomingForm();
+    form.multiples = true;
+    form.uploadDir = path.join(__dirname, '/uploads');
+
+    form.on('file', function (field, file) {
+        fs.rename(file.path, path.join(form.uploadDir, file.name));
+    });
+
+    form.on('error', function (err) {
+        console.log('An error has occured: \n' + err);
+    });
+
+    form.on('end', function () {
+        res.end('success');
+    });
+
+    form.parse(req);
+});
+
 
 app.use("/d/:id?", function (req, res, next) {
     if (!req.params.id) {
