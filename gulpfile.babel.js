cb53893ccac3f23e6948ccc9405c8885af099537
Thomas <Thomas@DevBook>
2017-02-06 20:08:11 +0100

    added gulp, not yet completed

---
+++ gulpfile.babel.js
@@ -0,0 +1,64 @@
+import gulp from "gulp";
+import sass from "gulp-sass";
+import sourcemaps from "gulp-sourcemaps";
+import rename from "gulp-rename";
+import babel from "gulp-babel";
+import browserify from "gulp-browserify";
+import nodemon from "gulp-nodemon";
+import runSequence from "gulp-run-sequence";
+
+gulp.task("scss", () => {
+  return gulp.src("./public/main.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(rename("main.min.css"))
+    .pipe(gulp.dest("./dist/public"));
+});
+// gulp.task("scss:watch", () => {
+//   gulp.watch("public/main.scss", ["scss"]);
+// });
+
+
+gulp.task("build-server", () => {
+  return gulp.src("server/*.js")
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("dist/server"));
+});
+// gulp.task("build-server:watch", () => {
+//   gulp.watch("server/*.js", ["build-server"]);
+// });
+
+
+gulp.task("build-client", () => {
+  return gulp.src("public/*.js")
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("dist/public"));
+});
+// gulp.task("build-client:watch", () => {
+//   gulp.watch("public/*.js", ["build-client"]);
+// });
+
+gulp.task("browserify", () => {
+  gulp.src("dist/public/main.js")
+    .pipe(browserify())
+    .pipe(gulp.dest("dist/public"))
+});
+// gulp.task("browserify:watch", () => {
+//   gulp.watch("dist/public/main.js", ["browserify"]);
+// });
+
+gulp.task("build", (done) => {
+  runSequence("build-client", "scss", "browserify", "build-server", done);
+})
+
+gulp.task('start', ["build"], () => {
+  nodemon({
+    script: 'dist/server/index.js'//,
+    // env: { 'NODE_ENV': 'development' }
+  })
+})
