21e7d5290520ac50e73b8f9cec87892a28fea1da
Thomas Klepzig <thomas.klepzig@comparex.de>
2017-08-08 09:30:41 +0200

    temporary add file input, needs refactoring

--- public/main.js
+++ public/main.js
@@ -10,11 +10,11 @@ if (id.length === 0) {
     if (typeof document.createElement("p").style.flex === "undefined") {
         $("body").addClass("no-flex-support");
     }
-    var input = document.createElement('input');
-    input.type = "file";
-    if (typeof input.files === "undefined") {
-        alert("File Upload not supported");
-    }
+    // var input = document.createElement('input');
+    // input.type = "file";
+    // if (typeof input.files === "undefined") {
+    //     alert("File Upload not supported");
+    // }
 })();
 
 function getFirstLine(text) {
@@ -124,6 +124,61 @@ $('body').bind('dragover', function (e) {
 
 $('#dropArea').bind('dragleave', function () {
     $('#dropArea').removeClass('visible');
+});
+
+$("#blubb").bind("change", function (e) {
+
+    var files = this.files;
+    
+    if (files.length > 0) {
+        var formData = new FormData();
+        for (var i = 0; i < files.length; i++) {
+            var file = files[i];
+            console.log(file.size);
+            formData.append('uploads[]', file, file.name);
+        }
+
+        $.ajax({
+            url: '/' + id + "/upload",
+            type: 'POST',
+            data: formData,
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: function () {
+                console.log('upload successful!');
+            },
+            xhr: function () {
+                // create an XMLHttpRequest
+                var xhr = new XMLHttpRequest();
+
+                // listen to the 'progress' event
+                xhr.upload.addEventListener('progress', function (evt) {
+
+                    if (evt.lengthComputable) {
+                        // calculate the percentage of upload completed
+                        var percentComplete = evt.loaded / evt.total;
+                        percentComplete = parseInt(percentComplete * 100);
+
+                        console.log(percentComplete + '%');
+                        // update the Bootstrap progress bar with the new percentage
+                        // $('.progress-bar').text(percentComplete + '%');
+                        // $('.progress-bar').width(percentComplete + '%');
+
+                        // once the upload reaches 100%, set the progress bar text to done
+                        if (percentComplete === 100) {
+                            console.log("done");
+                            // $('.progress-bar').html('Done');
+                        }
+
+                    }
+
+                }, false);
+
+                return xhr;
+            }
+        });
+    }
 });
 
 $('#dropArea').bind('drop', function (e) {
